6bd62886ad94bf0d66a0a9641d12bcef024d735a
MillanAir <millan.verma@gmail.com>
2016-01-15 17:14:07 -0500

    Initial Commit

---
+++ COMP397-JSBolierPlate-Winter16/Scripts/objects/person.ts
@@ -0,0 +1,16 @@
+﻿module objects {
+    export class Person {
+        //PRIVATE INSTANCE VARIABLE ++++++++++++++++++
+        protected _name: string;
+    
+        //CONSTRUCTOR ++++++++++++++++
+        constructor(name: string) {
+            this._name = name;
+        }
+
+        //PUBLIC METHODS
+        public speaks(): void {
+            console.log(this._name + " says hello");
+        }
+    }
+} 
